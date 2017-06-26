02920540eca3311a766010a0be1d621356e3344c
Eneymotorny <eneymotorny@gmail.com>
2017-06-26 05:09:42 +0300

    add functionality to group of checkboxradio

--- js/script.js
+++ js/script.js
@@ -11,7 +11,7 @@ $(function() {
 			$('<fieldset>').appendTo( $(target));
 		var $legend =
 			$('<legend>').appendTo($group);
-		var $fieldContainer =
+		var $fieldsContainer =
 			$('<div>').addClass('field-container').appendTo($group);
 		var $elemName;
 		addElemInp();
@@ -24,7 +24,7 @@ $(function() {
 			$('<label>Radio <input type="radio" name="selectType"></label>')
 				.appendTo($legend);
 		var $radCheckbox =
-			$('<label>Checkbox <input type="radio" name="selectType"></label>')
+			$('<label>Checkbox <input type="radio" checked name="selectType"></label>')
 				.appendTo($legend);
 		var $checkIcons =
 			$('<label>Иконки <input type="checkbox" name="with-icons"></label>')
@@ -32,11 +32,62 @@ $(function() {
 		var $btnSaveField =
 			$('<button class="btn-save">Сохранить группу</button>')
 				.on('click', function () {
-					var groupName = $('.inp-name-group').val();
-					if (groupName === null || groupName === '' || groupName.trim() === '') {
+					var group = {
+						name:  $groupName.children('input').val(),
+						radio: $radRadio.children('input').is(":checked"),
+						checkbox: $radCheckbox.children('input').is(":checked"),
+						icons: $checkIcons.children('input').is(":checked"),
+						fields: $fieldsContainer.find('input'),
+						hasEmptyFields: function () {
+							var has;
+							group.fields.each(function(indx, elem) {
+								has = $(elem).val() === null || $(elem).val().trim() === '';
+							});
+							return has
+						}
+					};
+
+					if (group.name === null || group.name.trim() === '') {
+						return
+					} else if (group.hasEmptyFields()) {
 						return
 					}
-					$legend.text(groupName)
+
+					$legend.text(group.name).addClass('ready');
+					$btnAddF.remove();
+
+					if (group.radio) {
+						(function () {
+							var idName = 'id' + (new Date().getTime());
+							if (group.fields.length === 1) {
+								$(group.fields[0])
+									.parent('label')
+									.text(group.fields[0].value)
+									.append('<input type="radio" checked name="' + idName + '" style=" display: none">')
+							}
+
+							for( var i = 0; i < group.fields.length; i++ ) {
+								$(group.fields[i])
+									.parent('label')
+									.text(group.fields[i].value)
+									.append('<input type="radio" name="' + idName + '">')
+							}
+						})()
+					}
+					if (group.checkbox) {
+						(function () {
+							var idName = 'id' + (new Date().getTime());
+							for( var i = 0; i < group.fields.length; i++ ) {
+								$(group.fields[i])
+									.parent('label')
+									.text(group.fields[i].value)
+									.append('<input type="checkbox" name="' + idName + '">')
+							}
+						})()
+					}
+					$group.find('[type=checkbox], [type=radio]').checkboxradio({
+						icon: group.icons
+					});
 				})
 				.appendTo($legend);
 		var $btnAddF =
@@ -49,28 +100,26 @@ $(function() {
 
 		function addElemInp() {
 			$elemName = $('<label><input class="inp-name-elem" type="text" maxlength="30" placeholder="Название элемента"></label>')
-				.appendTo($fieldContainer);
+				.appendTo($fieldsContainer);
 			countFields++
 		}
 
 // ==================== jQueryUI ====================
-		function addUserInterfase() {
-			$('[type=checkbox], [type=radio]').checkboxradio();
-			$('button.btn-save').button({
-				icon: "ui-icon-check",
-				showLabel: false
-			});
-			$('button.btn-add-new-f').button({
-				icon: "ui-icon-plusthick",
-				showLabel: false
-			});
-			$('button.btn-add-new-g').button({
-				icon: "ui-icon-plusthick",
-				showLabel: false
-			});
-			$('[name=selType]').selectmenu();
-		}
-		addUserInterfase();
+		$group.find('[type=checkbox], [type=radio]').checkboxradio();
+		$group.find('button.btn-save').button({
+			icon: "ui-icon-check",
+			showLabel: false
+		});
+		$group.find('button.btn-add-new-f').button({
+			icon: "ui-icon-plusthick",
+			showLabel: false
+		});
+		$group.find('button.btn-add-new-g').button({
+			icon: "ui-icon-plusthick",
+			showLabel: false
+		});
+		$group.find('[name=selType]').selectmenu();
+
 		countGroups++;
 	}
 
